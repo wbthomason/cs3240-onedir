94e5e0cc5581ad842ee809d369d271a8374a64fc
Dru Knox <knox.dru@gmail.com>
2013-11-03 17:45:38 -0500

    Added demo code for db_access

--- db_access.py
+++ db_access.py
@@ -32,7 +32,10 @@ def get_dirs(email):
 	cur.execute( get_dirs )
 	res = cur.fetchone()
 	
-	return res[0].split(',')
+	try:
+		return res[0].split(',')
+	except:
+		return []
 
 def set_dirs(email, dirs):
 	new_dirs = ""
@@ -62,18 +65,118 @@ def get_files(email):
 	cur.execute( get_files )
 	res = cur.fetchone()
 	
-	return res[0].split(',')
+	try:
+		return res[0].split(',')
+	except:
+		return []
 
-try:
-	#create_account("knox.dru@gmail.com", "password")
-    if login("knox.dru@gmail.com", "password"):
-		print get_dirs("knox.dru@gmail.com")
-		set_dirs("knox.dru@gmail.com", ["test/dir"])
-		print get_dirs("knox.dru@gmail.com")
-		add_dir("knox.dru@gmail.com", "test2/dir2")
-		print get_dirs("knox.dru@gmail.com")
-    
-except mdb.Error, e:
-  
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    sys.exit(1)
+if __name__ == "__main__":
+	loggedIn = False
+	user = ""
+	
+	print "###################################################"
+	print ""
+	print "Welcome to the local user account management system! Type a command to begin."
+	
+	print ""
+	print "###################################################"
+	print ""
+	
+	print "Available commands:"
+	print "create - creates a new local user account"
+	print "login - logs in to a given local user account"
+	print "logout - logs out of the current local user account"
+	print "add [arg1] - [arg1] specifies whether to add a file ([arg1] = file) or directory ([arg1] = dir)"
+	print "print [arg1] - [arg1] specifies whether to print all files ([arg1] = file) or all directories ([arg1] = dir)"
+	
+	print ""
+	print "###################################################"
+	while True:
+		print ""
+		input = raw_input("Enter a command: ").split()
+		cmd = input[0]
+		try:
+			arg1 = input[1]
+		except:
+			arg1 = ""
+			
+		if cmd == "create":
+			username = raw_input("Username: ")
+			password = raw_input("Password: ")
+			confirm  = raw_input("Confirm Password: ")
+			while password != confirm:
+				print "Passwords do not match! Please try again."
+				password = raw_input("Password: ")
+				confirm  = raw_input("Confirm Password: ")
+
+			try:
+				create_account(username, password)
+				print "Account created!"
+			
+			except mdb.Error, e:
+				print "Error %d: %s" % (e.args[0],e.args[1])
+		
+		elif cmd == "login":
+			username = raw_input("Username: ")
+			password = raw_input("Password: ")
+			
+			try:
+				while not login(username, password):
+					print "Incorrect username or password! Please try again."
+					username = raw_input("Username: ")
+					password = raw_input("Password: ")
+				
+				print "Login successful!"
+				loggedIn = True
+				user = username
+			
+			except mdb.Error, e:
+				print "Error %d: %s" % (e.args[0],e.args[1])
+		
+		elif cmd == "logout":
+			loggedIn = False
+			user = ""
+		
+		elif cmd == "add":
+			if loggedIn:
+				if arg1 == "dir":
+					dirName = raw_input("Path: ")
+					
+					try:
+						add_dir(user, dirName)
+						print "Directory added!"
+					
+					except mdb.Error, e:
+						print "Error %d: %s" % (e.args[0],e.args[1])
+				
+				if arg1 == "file":
+					fileName = raw_input("Path: ")
+					
+					try:
+						add_file(user, fileName)
+						print "Directory added!"
+					
+					except mdb.Error, e:
+						print "Error %d: %s" % (e.args[0],e.args[1])
+			else:
+				print "You must log in first."
+				
+		elif cmd == "print":
+			if loggedIn:
+				if arg1 == "dir":
+					try:
+						print get_dirs(user)
+					
+					except mdb.Error, e:
+						print "Error %d: %s" % (e.args[0],e.args[1])
+				
+				if arg1 == "file":
+					try:
+						print get_files(user)
+					
+					except mdb.Error, e:
+						print "Error %d: %s" % (e.args[0],e.args[1])
+			else:
+				print "You must log in first."
+		elif cmd == "quit":
+			sys.exit()
