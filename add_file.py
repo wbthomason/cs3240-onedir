644e294cfe44446e33975dbb5f6c61207956700f
jya <jy2an@virginia.edu>
2013-11-03 00:51:43 -0400

    File upload functionality

--- add_file.py
+++ add_file.py
@@ -1,7 +1,20 @@
 from twisted.web import client
 
-import os, tempfile, webbrowser, random
+import requests
+import db_access
+import MySQLdb as mdb
+import sys
 
 
 def upload_file():
-    return
+    url = 'http://posttestserver.com/post.php'
+    files = {'file': open(sys.argv[1])}
+    req = requests.post(url, files=files)
+
+if __name__ == "__main__":
+    try:
+        if db_access.login("knox.dru@gmail.com", "password"):
+            upload_file()
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0], e.args[1])
+        sys.exit(1)
