fc8bb40f48fe53ca1eb0f4aad1d9faf0d04e4f92
jya <jy2an@virginia.edu>
2013-11-14 07:58:13 -0500

    Change file upload to HTTP PUT. Still has to be integrated with other code.

--- add_file.py
+++ add_file.py
@@ -6,15 +6,16 @@ import MySQLdb as mdb
 import sys
 
 
-def upload_file():
-    url = 'http://posttestserver.com/post.php'
-    files = {'file': open(sys.argv[1])}
-    req = requests.post(url, files=files)
+def upload_file(File):
+    url = 'http://localhost:3240'
+    files = {'file': open(File)}
+    req = requests.put(url, files=files)
 
 if __name__ == "__main__":
+    file_to_upload = raw_input("File to upload?")
     try:
         if db_access.login("knox.dru@gmail.com", "password"):
-            upload_file()
+            upload_file(file_to_upload)
     except mdb.Error, e:
         print "Error %d: %s" % (e.args[0], e.args[1])
         sys.exit(1)
